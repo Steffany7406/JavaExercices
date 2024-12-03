df755f68e62930140311e7f04caab7dc4ec99ac9
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-12-03 18:23:06 -0300

    Add files via upload

---
+++ ArrayJava/Main.java
@@ -0,0 +1,19 @@
+import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Collection<String> vogais = new ArrayList<>();
+
+        vogais.add("A");
+        vogais.add("E");
+        vogais.add("I");
+        vogais.add("O");
+        vogais.add("U");
+
+        for (String vog : vogais){
+            System.out.println("Lista Atual: "+ vog);
+        }
+    }
+}
